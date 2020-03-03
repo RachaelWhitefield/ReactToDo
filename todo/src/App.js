8081a6459933737a1b423d168ba74ab7ff71f3e5
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-03-03 13:17:04 -0700

    added prop types to proper pages

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
 import About from "./components/pages/About";
-import uuid from 'react-uuid';
+// import uuid from 'react-uuid';
 import axios from "axios";
 import './App.css';
 
@@ -32,19 +32,20 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res =>  this.setState({ todos: [...this.state.todos.filter(todo => todo.id!== id)] }));
     // console.log(id)
-    this.setState({ todos: [...this.state.todos.filter(todo => todo.id!== id)] });
+   
   }
 
   // Add Todo
   addTodo = (title) => {
     // console.log(title)
-    const newTodo = {
-      id: uuid(),
+    axios.post("https://jsonplaceholder.typicode.com/todos", {
       title,
       completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo] });
+    })
+    .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
   }
 
   render() {
