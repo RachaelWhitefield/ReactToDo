95fbe1378d3fb6c3df0abc6e211f5f9a7cf35186
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-03-03 11:18:28 -0700

    added todo list add component and styling

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 import './App.css';
 
 class App extends Component {
@@ -45,8 +46,11 @@ class App extends Component {
     // console.log(this.state.todos)
     return (
       <div className="App>">
+        <div className="container">
         <Header />
+        <AddTodo />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>  
       </div>
     );
   }
